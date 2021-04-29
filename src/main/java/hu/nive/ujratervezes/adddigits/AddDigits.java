e803f268f70d4d5357794bc7188cbd47d9ddcc18
kubanekzsolt <63916757+kubanekzsolt@users.noreply.github.com>
2021-04-29 10:53:02 +0200

    AddDigits Class and method created

--- src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
@@ -1,5 +1,14 @@
 package hu.nive.ujratervezes.adddigits;
 
 public class AddDigits {
-    //todo: implement your solution here
+    public int addDigits(String digitString) {
+        if (digitString == null || digitString.length() == 0) {
+            return -1;
+        }
+        return digitString.chars()
+                .filter(c -> c >= '0')
+                .filter(c -> c <= '9')
+                .map(c -> c-'0')
+                .sum();
+    }
 }
